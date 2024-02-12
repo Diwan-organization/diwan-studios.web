b0b62b3f2112d0109c5ca4cfd2afa3a8d895aaa1
Sara Hossam <shossam@ultatel.com>
2024-02-12 11:21:27 +0200

    edit projects in homepage + add deft tab and pages

--- src/app/Common/Settings/RoutePaths.ts
+++ src/app/Common/Settings/RoutePaths.ts
@@ -2,7 +2,8 @@ export class RoutePaths {
 	public static Default = 'home';
 	public static Home = 'home';
 	public static ArtWorks = 'artworks';
+	public static Deft = 'deft';
 	public static About = 'about';
 	public static Contact = 'contactus';
-	
+
 }
