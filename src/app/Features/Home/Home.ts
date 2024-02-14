645b99e763f7c80f5d8b797712ac8003d078a0d8
Youssef Mansour <ymansour@ultatel.com>
2024-02-14 13:49:00 +0200

    add projects div scrolling strucutre and values basic structure

--- src/app/Features/Home/Home.ts
+++ src/app/Features/Home/Home.ts
@@ -3,8 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { animate, stagger } from 'motion';
 import { RoutePaths } from '@App/Common/Settings/RoutePaths';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CarouselComponent } from '@App/Common/Widgets/Carousel/Carousel';
+
+class ProjectItem {
+	ImgSrc!: string;
+	ImgAlt!: string;
+	Title!: string;
+	Description!: string;
+	Link!: string;
+}
 
 class NewsItem {
 	ImgSrc!: string;
@@ -15,8 +23,7 @@ class NewsItem {
 	Link!: string;
 }
 
-
-class AboutItem {
+class PartnerItem {
 	ImgSrc!: string;
 	ImgAlt!: string;
 	Title!: string;
@@ -31,6 +38,15 @@ class StatsItem {
 	Number!: string;
 	Description!: string;
 	Link!: string;
+	TimeInterval!: number;
+}
+
+class ValueItem {
+	ImgSrc!: string;
+	ImgAlt!: string;
+	Title!: string;
+	Description!: string;
+	Link!: string;
 }
 
 
@@ -41,6 +57,7 @@ class StatsItem {
 	imports: [FormsModule, CommonModule, RouterModule, CarouselComponent],
 })
 export class HomeComponent implements OnInit {
+
 	RoutePaths = RoutePaths;
 	observerOptions: {} = {
 		root: null,
@@ -56,14 +73,7 @@ export class HomeComponent implements OnInit {
 			Number: '15',
 			Description: '',
 			Link: '',
-		},
-		{
-			ImgSrc: '',
-			ImgAlt: '',
-			Title: 'Countries',
-			Number: '4',
-			Description: '',
-			Link: '',
+			TimeInterval: 100
 		},
 		{
 			ImgSrc: '',
@@ -72,18 +82,59 @@ export class HomeComponent implements OnInit {
 			Number: '25',
 			Description: '',
 			Link: '',
+			TimeInterval: 70
+		},
+		{
+			ImgSrc: '',
+			ImgAlt: '',
+			Title: 'Countries',
+			Number: '4',
+			Description: '',
+			Link: '',
+			TimeInterval: 200
+		},
+		{
+			ImgSrc: '',
+			ImgAlt: '',
+			Title: 'Learn more',
+			Number: '>',
+			Description: '',
+			Link: '/' + RoutePaths.About,
+			TimeInterval: 0
 		},
 	]
 
-	images: string[] = [
-		'assets/Images/girl-wall.jpg',
-		'assets/Images/dog-wall-2.jpg',
-		// 'assets/Images/girl-wall-2.jpg',
-		// 'assets/Images/girl-wall-3.jpg',
-		// 'assets/Images/girl-wall-4.jpg',
-		// 'assets/Images/girl-wall-5.jpg',
+	Projects: ProjectItem[] = [
+		{
+			ImgSrc: 'assets/Images/girl-wall.jpg',
+			ImgAlt: '',
+			Title: 'Fifa World Cup Qatar 2022',
+			Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat nemo, ullam quis corporis officia sint fugit, tempora quidem libero.',
+			Link: 'fifa',
+		},
+		{
+			ImgSrc: 'assets/Images/girl-wall-3.jpg',
+			ImgAlt: '',
+			Title: 'HSBC',
+			Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat nemo, ullam quis corporis officia sint fugit, tempora quidem libero.',
+			Link: 'hsbc',
+		},
+		{
+			ImgSrc: 'assets/Images/girl-wall.jpg',
+			ImgAlt: '',
+			Title: 'Moussa Hospital',
+			Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat nemo, ullam quis corporis officia sint fugit, tempora quidem libero.',
+			Link: 'moussa',
+		},
+		{
+			ImgSrc: 'assets/Images/girl-wall-3.jpg',
+			ImgAlt: '',
+			Title: 'Meryal Water Park',
+			Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat nemo, ullam quis corporis officia sint fugit, tempora quidem libero.',
+			Link: 'meryal',
+		}
 
-	]; // Add your image file names
+	];
 
 	News: NewsItem[] = [
 		{
@@ -112,24 +163,7 @@ export class HomeComponent implements OnInit {
 		}
 	]
 
-	About: AboutItem[] = [
-		{
-			ImgSrc: 'https://nyc.carouselartgroup.com/wp-content/uploads/2022/11/CFA-Atlanta-Gallery-jpeg.webp',
-			ImgAlt: 'about the artist',
-			Title: 'The Artist',
-			Description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-			Link: '',
-		},
-		{
-			ImgSrc: 'https://nyc.carouselartgroup.com/wp-content/uploads/2023/10/CHICAGO-GALLERY-jpg.webp',
-			ImgAlt: 'about the company',
-			Title: 'The Company',
-			Description: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-			Link: '',
-		}
-	]
-
-	Partners: AboutItem[] = [
+	Partners: PartnerItem[] = [
 		{
 			ImgSrc: 'assets/Logos/4seasons.png ',
 			ImgAlt: 'about the artist',
@@ -210,252 +244,164 @@ export class HomeComponent implements OnInit {
 	]
 	x = this.Partners.map(p => p.ImgSrc)
 
-	ngAfterViewInit() {
-		this.StatAnimation()
-	}
-
-	hoveredIndex: number | null = null;
-	StatAnimation() {
-		const number = document.querySelectorAll(".number");
-		const title = document.querySelectorAll(".stat-title");
-		const description = document.querySelectorAll(".description");
-
-		const observerOptions = {
-			root: null,
-			rootMargin: '0px',
-			threshold: 0.2
-		};
-
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					this.startAnimation(number, title, description);
-					observer.disconnect();
-				}
-			});
-		}, observerOptions);
-
-		if (number && number[0]) {
-			observer.observe(number[0]);
+	Values: ValueItem[] = [
+		{
+			ImgSrc: 'assets/Logos/4seasons.png ',
+			ImgAlt: 'about the artist',
+			Title: 'The Artist',
+			Description: '',
+			Link: '',
+		},
+		{
+			ImgSrc: 'assets/Logos/adidas.png ',
+			ImgAlt: 'about the company',
+			Title: 'The Company',
+			Description: '',
+			Link: '',
+		},
+		{
+			ImgSrc: 'assets/Logos/amazon.png ',
+			ImgAlt: 'about the company',
+			Title: 'The Company',
+			Description: '',
+			Link: '',
+		},
+		{
+			ImgSrc: 'assets/Logos/Careem2.png ',
+			ImgAlt: 'about the company',
+			Title: 'The Company',
+			Description: '',
+			Link: '',
 		}
+
+	]
+
+	constructor(private Router: Router) { }
+
+	ngOnInit(): void { }
+
+	ngAfterViewInit() {
+		this.Animation.LandingText();
+		this.Animation.About();
+		this.Animation.Stat();
+		this.Animation.Projects();
+		this.Animation.Partners();
 	}
 
-	startAnimation(number: any, title: any, description: any) {
-		setTimeout(() => {
-			number[0].setAttribute("style", "opacity:1");
-			animate(
-				(progress) => {
-					number[0].innerHTML = (Math.round(progress * 15)).toString();
-
-					if (progress === 1) {
-						number[0].innerHTML += "+";
+	Animation = {
+		LandingText: () => {
+			const landingText = document.querySelector('.landing-text')!;
+			landingText.classList.remove('landing-text-transition');
+			const landingTextObserver = new IntersectionObserver(entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						landingText.classList.add('landing-text-transition');
+						return;
 					}
-				},
-				{ duration: 0.5, easing: "ease-out" }
-			);
-			title[0].setAttribute("style", "opacity:1");
-			description[0].setAttribute("style", "opacity:1");
-			setTimeout(() => {
-				number[1].setAttribute("style", "opacity:1");
-				animate(
-					(progress) => {
-						number[1].innerHTML = (Math.round(progress * 4)).toString();
-					},
-					{ duration: 0.2, easing: "ease-out" }
-				);
-				title[1].setAttribute("style", "opacity:1");
-				description[1].setAttribute("style", "opacity:1");
 
-				setTimeout(() => {
-					number[2].setAttribute("style", "opacity:1");
-					animate(
-						(progress) => {
-							number[2].innerHTML = (Math.round(progress * 25)).toString();
-						},
-						{ duration: 0.5, easing: "ease-out" }
-					);
-					title[2].setAttribute("style", "opacity:1");
-					description[2].setAttribute("style", "opacity:1");
-				}, 500);
-			}, 700);
-		}, 700);
-	}
-
-	ngOnInit(): void {
-		const about = document.querySelector('.about')!;
-		about.classList.remove('about-transition');
-		const aboutObserver = new IntersectionObserver(entries => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					about.classList.add('about-transition');
-					return;
-				}
-
-				about.classList.remove('about-transition');
+					landingText.classList.remove('landing-text-transition');
+				});
 			});
-		});
-		aboutObserver.observe(about);
+			landingTextObserver.observe(landingText);
+		},
 
+		About: () => {
+			const about = document.querySelector('.about')!;
+			about.classList.remove('about-transition');
+			const aboutObserver = new IntersectionObserver(entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						about.classList.add('about-transition');
+						return;
+					}
 
-		const projects = document.querySelectorAll('.project')!;
-		projects.forEach(project => {
-			console.log(project);
+					about.classList.remove('about-transition');
+				});
+			});
+			aboutObserver.observe(about);
+		},
 
-			project.classList.remove('project-transition');
-
+		Stat: () => {
 			const observer = new IntersectionObserver(entries => {
 				entries.forEach(entry => {
 					if (entry.isIntersecting) {
-						project.classList.add('project-transition');
-						return;
+						animateNumber(entry.target);
+						// remove if you want it to repeat it on scroll again
+						observer.unobserve(entry.target);
 					}
-					project.classList.remove('project-transition');
 				});
 			});
+			const stats = document.querySelectorAll('.stat');
+			stats.forEach(stat => observer.observe(stat));
 
-			observer.observe(project);
-		})
+			function animateNumber(element: any) {
+				return new Promise<void>(resolve => {
+					const numberElement = element.querySelector('.number');
+					const targetNumber = parseInt(numberElement.getAttribute('number'));
+					const targetTimeInterval = parseInt(numberElement.getAttribute('timeinterval'));
+					console.log(targetNumber);
 
-		const partners = document.querySelector('.partners')!;
-		partners.classList.remove('partners-transition');
-		const partnersObserver = new IntersectionObserver(entries => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					partners.classList.add('partners-transition');
-					return;
-				}
+					let currentNumber = 0;
+					const interval = setInterval(() => {
+						if (!targetNumber) {
+							numberElement.innerText = numberElement.getAttribute('number');
+							clearInterval(interval);
+							resolve();
+							return
+						}
+						currentNumber++;
+						numberElement.innerText = currentNumber;
+						if (currentNumber >= targetNumber) {
+							if (numberElement.id == 0) {
+								numberElement.innerText += '+'
+							}
+							clearInterval(interval);
+							resolve();
+						}
+					}, targetTimeInterval);
+				});
+			}
+		},
 
-				partners.classList.remove('partners-transition');
+		Projects: () => {
+			const projects = document.querySelectorAll('.project')!;
+			projects.forEach(project => {
+				// console.log(project);
+				project.classList.remove('project-transition');
+
+				const observer = new IntersectionObserver(entries => {
+					entries.forEach(entry => {
+						if (entry.isIntersecting) {
+							project.classList.add('project-transition');
+							return;
+						}
+						project.classList.remove('project-transition');
+					});
+				});
+
+				observer.observe(project);
+			})
+		},
+
+		Partners: () => {
+			const partners = document.querySelector('.partners')!;
+			partners.classList.remove('partners-transition');
+			const partnersObserver = new IntersectionObserver(entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						partners.classList.add('partners-transition');
+						return;
+					}
+
+					partners.classList.remove('partners-transition');
+				});
 			});
-		});
-		partnersObserver.observe(partners);
-
-		// ///3wza agrb a3ml 3l elemnt msh section
-		// const sectionProjects: any = document.querySelector('.projects');
-		// const observerprojects = new IntersectionObserver((entries) => {
-		// 	entries.forEach((entry) => {
-		// 		if (entry.isIntersecting) {
-		// 			animate(
-		// 				'.gallery-img',
-		// 				{
-		// 					opacity: [0, 1],
-		// 					y: [-20, 0],
-
-		// 				},
-		// 				{
-		// 					delay: stagger(0.2),
-		// 					duration: 0.5,
-		// 					easing: ['ease-in-out'],
-		// 				}
-		// 			);
-		// 			animate(
-		// 				'.browse-more',
-		// 				{ opacity: [0, 1], y: [-40, 0] },
-		// 				{
-		// 					delay: stagger(0.6),
-		// 					duration: 0.5,
-		// 					easing: ['ease-in-out'],
-		// 				}
-		// 			);
-		// 			observerprojects.disconnect();
-		// 		}
-		// 	});
-		// }, this.observerOptions);
-		// observerprojects.observe(sectionProjects);
-
-
-
-
-		// // // Start observing the 'about' section
-		// // const sectionAbout: any = document.querySelector('.about');
-		// // const observerAbout = new IntersectionObserver((entries) => {
-		// // 	entries.forEach((entry) => {
-		// // 		if (entry.isIntersecting) {
-		// // 			// The 'about' section is in view, trigger animations
-		// // 			animate(
-		// // 				'.browse-more',
-		// // 				{ y: [-20, 0] },
-		// // 				{ delay: stagger(0.1), duration: 0.2 }
-		// // 			);
-		// // 			animate(
-		// // 				'.title',
-		// // 				{ y: [-20, 0] },
-		// // 				{ delay: stagger(0.1), duration: 0.2 }
-		// // 			);
-		// // 			animate(
-		// // 				'.sub-title',
-		// // 				{ y: [-20, 0] },
-		// // 				{ delay: stagger(0.1), duration: 0.2 }
-		// // 			);
-		// // 			animate(
-		// // 				'.tab-content',
-		// // 				{ y: [-20, 0] },
-		// // 				{ delay: stagger(0.1), duration: 0.9 }
-		// // 			);
-
-		// // 			// Disconnect the observer after triggering the animations once
-		// // 			observerNews.disconnect();
-		// // 		}
-		// // 	});
-		// // }, this.observerOptions);
-		// // observerAbout.observe(sectionAbout);
-
-
-
-
-		// // Start observing the 'latest-news' section
-		// const sectionLatestNews: any = document.querySelector('.latest-news');
-		// const observerNews = new IntersectionObserver((entries) => {
-		// 	entries.forEach((entry) => {
-		// 		if (entry.isIntersecting) {
-		// 			// The 'about' section is in view, trigger animations
-		// 			animate(
-		// 				'.card',
-		// 				{
-		// 					opacity: [0, 1],
-		// 					// x: [-20, 0],
-		// 					rotateY: ['-90deg', '0deg']
-		// 				},
-		// 				{
-		// 					delay: stagger(0.2),
-		// 					duration: 0.5,
-		// 					easing: ['ease-in-out'],
-		// 				}
-		// 			);
-		// 			animate(
-		// 				'.title',
-		// 				{ y: [-20, 0] },
-		// 			);
-		// 			// Disconnect the observer after triggering the animations once
-		// 			observerNews.disconnect();
-		// 		}
-		// 	});
-		// }, this.observerOptions);
-		// observerNews.observe(sectionLatestNews);
+			partnersObserver.observe(partners);
+		}
 	}
 
-	showVisibleDiv(index: number): void {
-		this.hoveredIndex = index;
-	}
-
-	hideVisibleDiv(index: number): void {
-		this.hoveredIndex = null;
-	}
-
-	BaseOpacity: number = 0.7;
-	opacity: number = 0.7;
-	@HostListener('window:scroll', ['$event'])
-	onWindowScroll() {
-		const scrollPosition: number = window.scrollY || document.documentElement.scrollTop;
-		const viewportHeight: number = window.innerHeight;
-
-		let ratio: number = scrollPosition / viewportHeight;
-		this.opacity = Math.min(this.BaseOpacity + (ratio / 3), 1);
-	}
-
-
-	MinIndex = 8;
-	loadMore() {
-		this.MinIndex = Math.min(this.MinIndex + 5, this.Partners.length);
+	GotoStat(link: string) {
+		if (!link) return
+		this.Router.navigateByUrl(link)
 	}
 }
